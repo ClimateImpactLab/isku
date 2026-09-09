70f4fe6f34dae85dba9023c41021a452e3a0623a
Brewster Malevich <bmalevich@rhg.com>
2026-09-09 16:46:54 -0700

    Cleanup to make ty/ruff happy with stricter rules

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -7,7 +7,7 @@ import xarray as xr
 import isku
 
 
-def test_basic_projection():
+def test_basic_projection() -> None:
     """
     Basic test running build_projection_template() with project().
     """
@@ -15,16 +15,16 @@ def test_basic_projection():
     params = xr.Dataset({"ni": (["idx"], [1, 2, 3])})
     expected = xr.Dataset({"impact": (["idx"], [13, 14, 15])})
 
-    def _pre(x):
+    def _pre(x: xr.Dataset) -> xr.Dataset:
         out = xr.Dataset()
         out["foobar"] = x["foobar"] + 1
         out["ni"] = x["ni"]
         return out
 
-    def _post(x):
+    def _post(x: xr.Dataset) -> xr.Dataset:
         return x[["impact"]] + 10
 
-    def _model(x):
+    def _model(x: xr.Dataset) -> xr.Dataset:
         return (x["foobar"] * 2 + x["ni"]).to_dataset(name="impact")
 
     test_impact_model = isku.build_projection_template(
